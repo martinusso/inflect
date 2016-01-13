e54731023163a39f176d4ae1d39470ce42d8949b
Breno Martinusso <martinusso@gmail.com>
2016-01-13 21:54:02 -0200

    Improve docs

--- ordinalize.go
+++ ordinalize.go
@@ -4,7 +4,13 @@ import "fmt"
 
 // Ordinalize turns a number into an ordinal string
 //
-// Ex: Ordinalize(1)     => "1st"
+// Ex: `Ordinalize(1)     => "1st"`
+//
+// ```Ordinalize(1)     => "1st"```
+//
+// ```
+// Ordinalize(1)     => "1st"
+// ```
 func Ordinalize(number int) string {
 	ordinal := Ordinal(number)
 	return fmt.Sprintf("%d%s", number, ordinal)
