145f92870ac7e3f62b0f4bb198771528ee0e5dde
Breno Martinusso <martinusso@gmail.com>
2016-01-14 20:32:57 -0200

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 /*
-Package inflect generate plurals of nouns, ordinals; ordinalize numbers; parameterize/asciify strings and convert numbers to words
+Package inflect generate plurals of nouns, ordinals; ordinalize numbers; parameterize/transliterate strings and convert numbers to words
 */
 package inflect
