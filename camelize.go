87ac8fbe53317098acf40e250214f2516fc90285
Breno Martinusso <martinusso@gmail.com>
2016-01-15 18:03:01 -0200

    Add lowerCamelCase method

--- camelize.go
+++ camelize.go
@@ -9,10 +9,17 @@ import (
 // Camelize converts strings to UpperCamelCase
 func Camelize(s string) string {
 	safe := Transliterate(s)
+	safe = strings.ToLower(safe)
 	reg, err := regexp.Compile("[^A-Za-z0-9]+")
 	if err != nil {
 		log.Fatal(err)
 	}
 	safe = reg.ReplaceAllString(safe, " ")
 	return strings.Replace(strings.Title(safe), " ", "", -1)
+}
+
+// LowerCamelize converts strings to lowerCamelCase
+func LowerCamelize(s string) string {
+	camelized := Camelize(s)
+	return strings.ToLower(string(camelized[0])) + camelized[1:]
 }
