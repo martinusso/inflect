2b10663d75def8bfc202998a43310df9f596a447
Breno Martinusso <martinusso@gmail.com>
2016-01-15 18:30:26 -0200

    refactor consonant and vowel

--- consonant_vowel.go
+++ consonant_vowel.go
@@ -1,29 +1,15 @@
 package inflect
 
-import "strings"
-
-var (
-	consonants = []string{"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n",
-		"p", "q", "r", "s", "t", "v", "x", "z", "w", "y"}
-	vowels = []string{"a", "e", "i", "o", "u"}
-)
+import "regexp"
 
 // IsConsonant check if a character is a consonant
 func IsConsonant(s string) bool {
-	for _, c := range consonants {
-		if c == strings.ToLower(s) {
-			return true
-		}
-	}
-	return false
+	matched, _ := regexp.MatchString("^[^aeiuo]+$", s)
+	return matched
 }
 
 // IsVowel check if a character is a vowel
 func IsVowel(s string) bool {
-	for _, v := range vowels {
-		if v == strings.ToLower(s) {
-			return true
-		}
-	}
-	return false
+	matched, _ := regexp.MatchString("^[aeiuo]+$", s)
+	return matched
 }
--- consonant_vowel_test.go
+++ consonant_vowel_test.go
@@ -3,17 +3,28 @@ package inflect
 import "testing"
 
 func TestConsonant(t *testing.T) {
+	consonants := []string{"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n",
+		"p", "q", "r", "s", "t", "v", "x", "z", "w", "y"}
+
 	for _, c := range consonants {
 		if !IsConsonant(c) {
 			t.Errorf("Expected '%s' is a consonant got false", c)
 		}
 	}
+	if IsConsonant("abc") {
+		t.Errorf("'abc' should not be considered consonant")
+	}
 }
 
 func TestVowel(t *testing.T) {
+	vowels := []string{"a", "e", "i", "o", "u"}
+
 	for _, v := range vowels {
 		if !IsVowel(v) {
 			t.Errorf("Expected '%s' is a vowel got false", v)
 		}
+	}
+	if IsVowel("abc") {
+		t.Errorf("'abc' should not be considered vowel")
 	}
 }
